4c960ec3acbecf36eadd20ef6891f430c7587204
Anupam Ashish Minz <anupamminz300am@gmail.com>
2023-02-12 10:23:10 +0530

    get ui list from list of musics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 use rodio::{Decoder, OutputStream, PlayError, Sink, StreamError};
-use std::fs::File;
-use std::io::BufReader;
+use std::{
+    error::Error,
+    fs::{self, File},
+    io::{BufRead, BufReader},
+    path::PathBuf,
+    thread,
+    time::Duration,
+};
+use tui::{
+    backend::CrosstermBackend,
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, List, ListItem, ListState},
+    Terminal,
+};
 
 #[derive(Debug)]
 enum AudioError {
@@ -45,17 +62,20 @@ impl From<rodio::decoder::DecoderError> for AudioError {
     }
 }
 
+#[allow(dead_code)]
 fn play_audio() -> Result<(), AudioError> {
-    let file_list = ["assets/test.ogg", "assets/test.ogg"];
+    let songlist = BufReader::new(File::open("assets/songlist")?);
     let (_stream, stream_handler) = OutputStream::try_default()?;
 
-    for f in file_list {
-        let file = BufReader::new(File::open(f)?);
+    for (i, f) in songlist.lines().enumerate() {
+        let file = BufReader::new(File::open(f?)?);
 
         let source = Decoder::new(file)?;
         let sink = Sink::try_new(&stream_handler)?;
 
         sink.append(source);
+
+        println!("playing song {}", i + 1);
 
         sink.sleep_until_end();
     }
@@ -63,9 +83,109 @@ fn play_audio() -> Result<(), AudioError> {
     return Ok(());
 }
 
-fn main() {
-    match play_audio() {
-        Ok(()) => (),
-        Err(e) => panic!("{}", e),
-    };
+#[allow(dead_code)]
+fn draw_lists(list: Vec<String>) -> Result<(), std::io::Error> {
+    let list_items: Vec<_> = list.iter().map(|x| ListItem::new(&x[..])).collect();
+    let mut list_state = ListState::default();
+    let mut selection_i: Option<usize> = None;
+
+    enable_raw_mode()?;
+
+    let mut stdout = std::io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    loop {
+        thread::sleep(Duration::from_millis(15));
+        terminal.draw(|f| {
+            let size = f.size();
+
+            let list = List::new(list_items.clone())
+                .block(Block::default().title("List").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+                .highlight_symbol(">>");
+
+            f.render_stateful_widget(list, size, &mut list_state);
+        })?;
+        if crossterm::event::poll(Duration::from_millis(300))? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => break,
+                    KeyCode::Char('j') => {
+                        // move to next item in list
+                        if let Some(i) = selection_i {
+                            if i < list_items.len() - 1 {
+                                selection_i = Some(i + 1);
+                            }
+                        } else {
+                            selection_i = Some(0);
+                        }
+                        list_state.select(selection_i);
+                    }
+                    KeyCode::Char('k') => {
+                        // move to prev item in list
+                        if let Some(i) = selection_i {
+                            if i > 0 {
+                                selection_i = Some(i - 1);
+                            }
+                        }
+                        list_state.select(selection_i);
+                    }
+                    KeyCode::Char('g') => {
+                        // move to top of the list
+                        selection_i = Some(0);
+                        list_state.select(selection_i);
+                    }
+                    KeyCode::Char('G') => {
+                        // move to bottom of the list
+                        selection_i = Some(list_items.len() - 1);
+                        list_state.select(selection_i);
+                    }
+                    KeyCode::Esc | KeyCode::Char('x') => {
+                        // unselect
+                        selection_i = None;
+                        list_state.select(selection_i);
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+
+    terminal.show_cursor()?;
+    Ok(())
+}
+
+fn get_file_list(root: PathBuf, list: &mut Vec<PathBuf>) {
+    for file in fs::read_dir(root).unwrap() {
+        let file = file.unwrap();
+        let file_type = file.file_type().unwrap();
+        if file_type.is_file() {
+            list.push(file.path());
+        } else if file_type.is_dir() {
+            get_file_list(file.path(), list);
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut file_list = Vec::new();
+    get_file_list(PathBuf::from("assets"), &mut file_list);
+
+    let file_list: Vec<String> = file_list
+        .iter()
+        .map(|x| x.to_str().unwrap().to_string())
+        .collect();
+
+    draw_lists(file_list)?;
+    return Ok(());
 }
